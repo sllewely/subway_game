64d0b3453fc8cf64c80da9a2cbe8b8f70b2b1a1b
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-04-13 23:30:04 -0400

    swipe the card movement

---
+++ Assets/TEAM_ASSETS/SCRIPTS/SwipeCardBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeCardBehavior : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // TODO: Detect if card would pass through stuff and prevent
+            Debug.Log("touch");
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            transform.Translate(-touchDeltaPosition.x * speed, 0, -touchDeltaPosition.y * speed);
+        }
+
+    }
+}
