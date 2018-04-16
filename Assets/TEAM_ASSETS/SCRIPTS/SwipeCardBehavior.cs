b0d9eca9f03141be295e0b76c2e23fa3f928a2d4
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-04-15 22:03:35 -0400

    the card moves between the sliders

--- Assets/TEAM_ASSETS/SCRIPTS/SwipeCardBehavior.cs
+++ Assets/TEAM_ASSETS/SCRIPTS/SwipeCardBehavior.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SwipeCardBehavior : MonoBehaviour {
     public float speed;
+    public GameObject leftSlider;
+    public GameObject rightSlider;
 
 	// Use this for initialization
 	void Start () {
@@ -15,13 +17,28 @@ public class SwipeCardBehavior : MonoBehaviour {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             // TODO: Detect if card would pass through stuff and prevent
-            Debug.Log("touch");
+            // Debug.Log("touch");
             // Get movement of the finger since last frame
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 
             // Move object across XY plane
-            transform.Translate(-touchDeltaPosition.x * speed, 0, -touchDeltaPosition.y * speed);
+            Vector3 newPos = transform.position - new Vector3(touchDeltaPosition.x * speed, 0, touchDeltaPosition.y * speed);
+            if (pointOkay(newPos))
+            {
+                transform.position = newPos;
+            }
         }
 
+    }
+
+    private bool pointOkay(Vector3 point)
+    {
+        Vector3 offsetPoint = point - Vector3.up;
+        if (leftSlider.GetComponent<Collider>().bounds.Contains(offsetPoint) || rightSlider.GetComponent<Collider>().bounds.Contains(offsetPoint))
+        {
+            // Debug.Log("Inside left slider " + point);
+            return false;
+        }
+        return true;
     }
 }
