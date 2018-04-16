3e76e5cd9952c58445850e4efd7de6fe8f41b7a3
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-04-16 15:00:00 -0400

    functional list operation

--- Assets/TEAM_ASSETS/SCRIPTS/SwipeCardBehavior.cs
+++ Assets/TEAM_ASSETS/SCRIPTS/SwipeCardBehavior.cs
@@ -16,8 +16,6 @@ public class SwipeCardBehavior : MonoBehaviour {
 	void Update () {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
-            // TODO: Detect if card would pass through stuff and prevent
-            // Debug.Log("touch");
             // Get movement of the finger since last frame
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
 
@@ -31,29 +29,14 @@ public class SwipeCardBehavior : MonoBehaviour {
 
     }
 
+    // Stop card from passing through sliders
     private bool moveOkay(Vector3 translation)
-    {
-        /***
-        return bottomBounds(transform.position).Exists(vertex =>
+    {  
+        return !bottomBounds(transform.position + translation).Exists(vertex =>
          {
-             Vector3 futurePoint = vertex + translation;
-             return leftSlider.GetComponent<Collider>().bounds.Contains(futurePoint) || rightSlider.GetComponent<Collider>().bounds.Contains(futurePoint);
-         }); ***/
-        foreach(Vector3 vertex in bottomBounds(transform.position + translation))
-        {
-            Debug.Log("vertex is " + vertex);
-            if (leftSlider.GetComponent<Collider>().bounds.Contains(vertex))
-            {
-                Debug.Log("inside left");
-                return false;
-            }
-            if (rightSlider.GetComponent<Collider>().bounds.Contains(vertex))
-            {
-                Debug.Log("inside right");
-                return false;
-            }
-        }
-        return true;
+             // Check if point inside either slider
+             return leftSlider.GetComponent<Collider>().bounds.Contains(vertex) || rightSlider.GetComponent<Collider>().bounds.Contains(vertex);
+         });
     }
 
     // Get all lower vertices of the metro card
