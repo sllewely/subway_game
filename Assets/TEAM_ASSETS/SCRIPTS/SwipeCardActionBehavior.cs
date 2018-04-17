a5589e05a3281f91139f21213684c73b7e0a3320
Sarah Llewelyn <se.llewelyn@gmail.com>
2018-04-17 09:17:48 -0400

    swipe detection box

---
+++ Assets/TEAM_ASSETS/SCRIPTS/SwipeCardActionBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeCardActionBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter " + other.name);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        Debug.Log("stay " + other.name);
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exit " + other.name);
+    }
+}
