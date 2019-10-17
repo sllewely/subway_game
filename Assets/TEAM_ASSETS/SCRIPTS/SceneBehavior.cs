e1284175b566f0b2622f6ecd4839bf6c8d0c61e8
Sarah Llewelyn <se.llewelyn@gmail.com>
2019-10-16 21:08:04 -0400

    balance decreases with each swipe

--- Assets/TEAM_ASSETS/SCRIPTS/SceneBehavior.cs
+++ Assets/TEAM_ASSETS/SCRIPTS/SceneBehavior.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneBehavior : MonoBehaviour
 {
+    public float startingBalance;
+    public float swipeCost;
+
+    public TMP_Text debugMessage;
+    
     private Scene thisScene;
     
     // Start is called before the first frame update
     void Start()
     {
+//        PersistData.Balance = startingBalance;
         thisScene = SceneManager.GetActiveScene();
     }
 
@@ -21,11 +28,13 @@ public class SceneBehavior : MonoBehaviour
 
     public void Reset()
     {
+        debugMessage.text = PersistData.Balance.ToString();
         StartCoroutine(reloadWithDelay());
     }
 
     private IEnumerator reloadWithDelay()
     {
+        PersistData.Balance -= swipeCost;
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(thisScene.name);
     }
